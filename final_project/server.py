e475c65eb2e2df1ffc4596c5c2bc3186e0d58a9c
Your Name <you@example.com>
2023-06-18 19:19:15 +0200

    Final changes

--- final_project/server.py
+++ final_project/server.py
@@ -1,7 +1,7 @@
 from machinetranslation import translator
 from flask import Flask, render_template, request
 import json
-from machinetranslation import * 
+from machinetranslation import translator
 
 app = Flask("Web Translator")
 
@@ -9,17 +9,17 @@ app = Flask("Web Translator")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
-    return "Translated text to French"
+    return translator.english_to_french(textToTranslate)
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
-    return "Translated text to English"
+    return translator.french_to_english(textToTranslate)
 
 @app.route("/")
 def renderIndexPage():
-    return render_template("/template/index.html")
+    return render_template("index.html")
 
     # Write the code to render template
 
